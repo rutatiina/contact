cf686c67a98d20ab2319796dabe7420e3f4ad4b6
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-10-11 22:49:23 +0300

    Create seeder files

---
+++ src/Seeders/ContactsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rutatiina\Contact\Seeders;
+
+use Illuminate\Database\Seeder;
+use Rutatiina\Contact\Models\Contact;
+
+class ContactsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
